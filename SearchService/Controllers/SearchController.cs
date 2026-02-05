aaae7735556fb2ba9c1947cda4e0d117ec86b4a1
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2026-02-05 10:33:39 +0200

    api/search

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearctItems(string searchTerms)
+        {
+            var query = DB.Default.Find<Item>();
+            query.Sort(x => x.Ascending(a => a.Model));
+            if (!string.IsNullOrWhiteSpace(searchTerms))
+            {
+                query.Match(Search.Full, searchTerms).SortByTextScore();
+            }
+            var result = await query.ExecuteAsync();
+            return Ok(result);
+        }
+    }
+}
