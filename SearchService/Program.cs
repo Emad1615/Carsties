295fdcdac0f1dc802e2138f22138374e5158645a
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2026-02-07 22:12:26 +0200

    http polly extensions

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,5 +1,8 @@
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>()
+    .AddPolicyHandler(GetPolicy())
+    .AddPolicyHandler(GetCircuitBreakerPolicy());
 
 var app = builder.Build();
 
@@ -30,3 +35,19 @@ catch (Exception ex)
 }
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions.HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+//.WaitAndRetryAsync(
+//    retryCount: 3,
+//     sleepDurationProvider:retryAttem=> TimeSpan.FromMilliseconds(200* Math.Pow(2,retryAttem))
+//    );
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy() =>
+    HttpPolicyExtensions.HandleTransientHttpError()
+    .CircuitBreakerAsync(
+            handledEventsAllowedBeforeBreaking: 5,
+            durationOfBreak: TimeSpan.FromSeconds(30)
+        );
